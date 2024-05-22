1bd2450d023803a57f4411c7db193dc005dfe65d
Nobsmoke123 <donaldakobundu@gmail.com>
2024-05-22 21:41:36 +0100

    Completed user crud

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,4 +1,6 @@
+const { updatePasswordSchema } = require('../validators/validation_schemas');
 const UserModel = require('./../model/user.model');
+const bcrypt = require('bcrypt');
 
 class UserController {
   async getUser(req, res) {
@@ -15,6 +17,61 @@ class UserController {
     return res.status(200).json({
       msg: 'User profile',
       user,
+    });
+  }
+
+  async updateUser(req, res) {
+    const userId = req.user._id;
+    const { name, photo, bio, phone } = req.body;
+    const user = await UserModel.findOneAndUpdate(
+      { _id: userId },
+      { name, photo, bio, phone },
+      { new: true }
+    ).select('-password');
+
+    if (!user) {
+      return res.status(401).json({
+        msg: 'User not authorized.',
+      });
+    }
+
+    return res.status(200).json({
+      msg: 'User profile updated',
+      user,
+    });
+  }
+
+  async updatePassword(req, res) {
+    await updatePasswordSchema.validateAsync(req.body);
+
+    const userId = req.user._id;
+
+    const { old_password, new_password } = req.body;
+
+    const user = await UserModel.findOne({ _id: userId });
+
+    if (!user) {
+      return res.status(401).json({
+        msg: 'User not authorized.',
+      });
+    }
+
+    const status = await bcrypt.compare(old_password, user.password);
+
+    console.log(`The status is ${status}`);
+
+    if (!(await bcrypt.compare(old_password, user.password))) {
+      return res.status(401).json({
+        msg: 'Old password is invalid.',
+      });
+    }
+
+    user.password = new_password;
+
+    await user.save();
+
+    return res.status(200).json({
+      msg: 'Password updated successfully.',
     });
   }
 }
